964f0c0d9dae0b6228e1010e1d4d68b8e0d2b2b6
Kuba Jasko <kubajasko@hotmail.co.uk>
2021-02-23 22:54:23 +0000

    Added ansible templating

--- setup.sh
+++ setup.sh
@@ -2,10 +2,24 @@
 
 STACKS="$(ls stacks)"
 
+OPT_DEPLOY=""
+OPT_REMOVE=""
+while getopts dr opt; do
+    case $opt in
+        d)  OPT_DEPLOY="yes" ;;
+        r)  OPT_REMOVE="yes" ;;
+        \?) echo "Invalid option -$OPTARG!" >&2
+            exit 1 ;;
+        :)  echo "Option -$OPTARG requires an argument." >&2
+            exit 1 ;;
+    esac
+done
+shift $((OPTIND - 1))
+
 function logg_error {
     for line in "$@"
     do
-        echo "ERROR: $line"
+        printf "ERROR: %s\n" "$line"
     done
     exit 3
 }
@@ -15,12 +29,23 @@ STACK="$1"
 
 if [[ -z "$STACK" ]]
 then
-    logg_error "Need to provide a stack file!" "Valid stacks: $STACKS"
+    logg_error "Need to provide a stack file!" "Valid stacks:" $STACKS
 fi
 if [[ ! -d "stacks/$STACK" ]]
 then
-    logg_error "Stack file does not exist: '$STACK'" "Valid stacks: $STACKS"
+    logg_error "Stack file does not exist: '$STACK'" "Valid stacks:" $STACKS
 fi
 
 cd "stacks/$STACK"
-docker stack deploy --compose-file docker-compose.yaml "$STACK"
+if [[ "$OPT_DEPLOY" && "$OPT_REMOVE" ]]
+then
+    docker stack rm "$STACK"
+elif [[ "$OPT_DEPLOY" ]]
+then
+    docker stack deploy --compose-file docker-compose.yaml "$STACK"
+elif [[ "$OPT_REMOVE" ]]
+then
+    docker-compose down
+else
+    docker-compose up -d
+fi
